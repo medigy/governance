3d8476a1a74e172b53836a64d40bf950c61ab8b2
Shahid N. Shah <shahid@shah.org>
2020-09-24 18:57:36 -0400

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -5,7 +5,7 @@
 export * as fs from "https://deno.land/std@v0.70.0/fs/mod.ts";
 export * as path from "https://deno.land/std@v0.70.0/path/mod.ts";
 export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.1/src/docopt.ts";
-export * as governedInstr from "https://denopkg.com/gov-suite/governed-instrument@v0.6.0/core/mod.ts";
+export * as governedInstr from "https://denopkg.com/gov-suite/governed-instrument@v0.7.0/core/mod.ts";
 export * as serializeJS from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/index.ts";
 export * as serializeJsStringify from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/stringify.ts";
 export * as serializeJsTypes from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/types.ts";
---
+++ mod_test.ts
@@ -0,0 +1,4 @@
+// TODO until unit tests are added, we'll just run a compile
+import {} from "./cli.ts";
+import {} from "./code.ts";
+import {} from "./quantitative-evaluation/mod.ts";
