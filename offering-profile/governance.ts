5257441fd47996b4b7df5eaf2477eefb46ef427d
Shahid N. Shah <shahid@shah.org>
2020-09-25 22:19:59 -0400

    feat: initial elaboration of offering schema

--- offering-profile/governance.ts
+++ offering-profile/governance.ts
@@ -1,13 +1,40 @@
 import { governedInstr } from "./deps.ts";
 
-// deno-lint-ignore no-empty-interface
-export interface IndividualProfileConstructionContext
+export interface OfferingProfileConstructionContext
   extends governedInstr.TypicalInstrumentOptions {
+  readonly nature: OfferingNature;
 }
 
-export interface IndividualProfileConstructor {
-  new (ctx: IndividualProfileConstructionContext): IndividualProfile;
+export interface OfferingProfileConstructor {
+  new (ctx: OfferingProfileConstructionContext): OfferingProfile;
 }
 
-export class IndividualProfile extends governedInstr.TypicalInstrument {
+export interface OfferingNature {
+  readonly isOffering: true;
+}
+
+export interface ProductOffering extends OfferingNature {
+  readonly isProductOffering: true;
+}
+
+export interface ServiceOffering extends OfferingNature {
+  readonly isServiceOffering: true;
+}
+
+export interface SolutionOffering extends ProductOffering, ServiceOffering {
+  readonly isSolutionOffering: true;
+}
+
+export interface InfrastructureOffering extends ProductOffering {
+  readonly isInfrastructureOffering: true;
+}
+
+export interface ApiOffering extends ProductOffering {
+  readonly isApiOffering: true;
+}
+
+export class OfferingProfile extends governedInstr.TypicalInstrument {
+  constructor(opcc: OfferingProfileConstructionContext) {
+    super(opcc);
+  }
 }
