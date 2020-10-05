60414f46f79a551f224b846cb8fc848aa0e14ed3
Geo V L <geovlazar@outlook.com>
2020-10-05 14:41:30 +0530

    Update Validation Rules for Offering Profile

--- offering-profile/lform.ts
+++ offering-profile/lform.ts
@@ -1,6 +1,36 @@
 import { govnData as gd, nihLhcForms as lf } from "./deps.ts";
 
-export interface RespondentCompanyName extends lf.UniqueTextItem {
+export interface MultiLineTextItem extends lf.FormItem {
+  readonly dataType: "TX";
+}
+
+export function isMultiLineTextItem(i: lf.FormItem): i is MultiLineTextItem {
+  return i.dataType == "TX";
+}
+
+export type UniqueMultiLineTextItem = MultiLineTextItem & lf.UniqueItem;
+
+export interface DateItem extends lf.FormItem {
+  readonly dataType: "DT";
+}
+
+export function isDateItem(i: lf.FormItem): i is DateItem {
+  return i.dataType == "DT";
+}
+
+export type UniqueDateItem = DateItem & lf.UniqueItem;
+
+export interface RequiredSingleAnswer {
+  readonly answerCardinality: { min: 0 | 1; max: 1 };
+}
+
+export interface RequiredMultipleAnswers {
+  readonly answerCardinality: { min: 1; max: "*" };
+}
+
+export type RequiredUniqueTextItem = lf.UniqueTextItem & RequiredSingleAnswer;
+
+export interface RespondentCompanyName extends RequiredUniqueTextItem {
   readonly questionCode: "company-name";
   readonly localQuestionCode: "company-name";
   readonly question: "Name of the company providing this offering*";
@@ -18,7 +48,7 @@ export interface RespondentContactPhoneNumber extends lf.UniquePhoneItem {
   readonly question: "Contact number of the company*";
 }
 
-export interface RespondentVendorName extends lf.UniqueTextItem {
+export interface RespondentVendorName extends RequiredUniqueTextItem {
   readonly questionCode: "Q002-07";
   readonly localQuestionCode: "Q002-07";
   readonly question: "Vendor Name*";
@@ -60,11 +90,128 @@ export interface RespondentContactInformation extends lf.FormItem {
 export interface ProductDetails extends lf.FormItem {
   readonly header: true;
   readonly question: "Product Details";
+  readonly items: [
+    OfferingType,
+    OfferingOwnerCheck,
+    OfferingTopics,
+    OfferingName,
+    OfferingOneLinerDescription,
+    OfferingFeaturedProductCheck,
+    OfferingCreatedDate,
+    OfferingWebsite,
+    OfferingLicense,
+    OfferingGitRepository,
+    OfferingDescription,
+    OfferingPermaLink,
+  ];
+}
+
+export interface OfferingType extends lf.ConstrainedListItem {
+  readonly questionCode: "Q005-14";
+  readonly localQuestionCode: "Q005-14";
+  readonly question: "Type of offering*";
+}
+
+export interface OfferingOwnerCheck extends lf.ConstrainedListItem {
+  readonly questionCode: "Q005-18";
+  readonly localQuestionCode: "Q005-18";
+  readonly question: "Are you the owner*";
+}
+
+export interface OfferingTopics extends lf.ConstrainedListItem {
+  readonly questionCode: "Q005-01";
+  readonly localQuestionCode: "Q005-01";
+  readonly question: "Select the categories that best describe the offering*";
+}
+
+export interface OfferingName extends RequiredUniqueTextItem {
+  readonly questionCode: "Q005-02";
+  readonly localQuestionCode: "Q005-02";
+  readonly question: "Offering name*";
+}
+
+export interface OfferingOneLinerDescription extends UniqueMultiLineTextItem {
+  readonly questionCode: "Q005-11";
+  readonly localQuestionCode: "Q005-11";
+  readonly question: "A one liner describing the offering";
+}
+
+export interface OfferingFeaturedProductCheck extends lf.ConstrainedListItem {
+  readonly questionCode: "Q005-12";
+  readonly localQuestionCode: "Q005-12";
+  readonly question: "Feature Product";
+}
+
+export interface OfferingCreatedDate extends UniqueDateItem {
+  readonly questionCode: "Q005-13";
+  readonly localQuestionCode: "Q005-13";
+  readonly question: "Created On*";
+}
+
+export interface OfferingWebsite extends lf.UniqueTextItem {
+  readonly questionCode: "Q005-03";
+  readonly localQuestionCode: "Q005-02";
+  readonly question: "Website of the offering";
+}
+
+export interface OfferingLicense extends lf.ConstrainedListItem {
+  readonly questionCode: "Q005-22";
+  readonly localQuestionCode: "Q005-22";
+  readonly question: "License Of The Offering";
+}
+
+export interface OfferingGitRepository extends lf.UniqueTextItem {
+  readonly questionCode: "Q005-06";
+  readonly localQuestionCode: "Q005-06";
+  readonly question: "GIT repository link of the offering";
+}
+
+export interface OfferingDescription extends UniqueMultiLineTextItem {
+  readonly questionCode: "Q005-07";
+  readonly localQuestionCode: "Q005-07";
+  readonly question:
+    "Describe the key benefits and unique value proposition of the offering";
+}
+
+export interface OfferingPermaLink extends lf.UniqueTextItem {
+  readonly questionCode: "Q005-10";
+  readonly localQuestionCode: "Q005-10";
+  readonly question: "Permalink";
 }
 
 export interface SocialPresence extends lf.FormItem {
   readonly header: true;
   readonly question: "Social Presence";
+  readonly items: [
+    SocialPresenceFacebookLink,
+    SocialPresenceTwitterLink,
+    SocialPresenceLinkedInLink,
+    SocialPresenceInstagramLink,
+  ];
+}
+
+export interface SocialPresenceFacebookLink extends lf.UniqueTextItem {
+  readonly questionCode: "Q006-01";
+  readonly localQuestionCode: "Q006-01";
+  readonly question: "Facebook page of the offering";
+}
+
+export interface SocialPresenceTwitterLink extends lf.UniqueTextItem {
+  readonly questionCode: "Q006-02";
+  readonly localQuestionCode: "Q006-02";
+  readonly question: "Twitter page of the offering";
+}
+
+export interface SocialPresenceLinkedInLink extends lf.UniqueTextItem {
+  readonly questionCode: "Q006-03";
+  readonly localQuestionCode: "Q006-03";
+  readonly question: "LinkedIn page of the offering";
+}
+
+export interface SocialPresenceInstagramLink extends lf.UniqueTextItem {
+  readonly questionCode: "Q006-04";
+  readonly localQuestionCode: "Q006-04";
+  readonly question: "Instagram page of the offering";
 }
 
 export interface OfferingProfileLhcForm extends lf.NihLhcForm {
