2be3e0f4dcbd880ad01b9739e2c1e8ddb4c6b0fb
Shahid N. Shah <shahid@shah.org>
2020-10-13 15:52:49 -0400

    fix: update dependencies

--- deps.ts
+++ deps.ts
@@ -2,16 +2,16 @@
 // is required only for a single module, those will be in <module>/deps.ts
 // subdirectory
 
-export * as fs from "https://deno.land/std@v0.73.0/fs/mod.ts";
-export * as path from "https://deno.land/std@v0.73.0/path/mod.ts";
-export * as governedInstr from "https://denopkg.com/gov-suite/governed-instrument@v0.7.3/core/mod.ts";
-export * as govnDataCLI from "https://denopkg.com/gov-suite/governed-structured-data@v0.6.0/cli.ts";
-export * as govnData from "https://denopkg.com/gov-suite/governed-structured-data@v0.6.0/mod.ts";
+export * as fs from "https://deno.land/std@v0.74.0/fs/mod.ts";
+export * as path from "https://deno.land/std@v0.74.0/path/mod.ts";
+export * as governedInstr from "https://denopkg.com/gov-suite/governed-instrument@v0.7.5/core/mod.ts";
+export * as govnDataCLI from "https://denopkg.com/gov-suite/governed-structured-data@v0.6.1/cli.ts";
+export * as govnData from "https://denopkg.com/gov-suite/governed-structured-data@v0.6.1/mod.ts";
 export * as serializeJS from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/index.ts";
 export * as serializeJsStringify from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/stringify.ts";
 export * as serializeJsTypes from "https://denopkg.com/shah/javascript-stringify@v2.0.4/src/types.ts";
 export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
 export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.5.1/mod.ts";
 export * as inspText from "https://denopkg.com/shah/ts-inspect@v0.5.1/text/mod.ts";
-export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v1.5.2/mod.ts";
+export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v1.5.3/mod.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.2.4/mod.ts";
