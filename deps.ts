6b3af6043313aa664ad5be8218fa2a7b374e6881
Shahid N. Shah <shahid@shah.org>
2020-10-15 16:16:31 -0400

    fix: update dependencies

--- deps.ts
+++ deps.ts
@@ -14,5 +14,5 @@ export * as serializeJsTypes from "https://denopkg.com/shah/javascript-stringify
 export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
 export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.6.6/mod.ts";
 export * as inspText from "https://denopkg.com/shah/ts-inspect@v0.6.6/text/mod.ts";
-export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v1.5.10/mod.ts";
+export * as nihLhcForms from "https://denopkg.com/shah/ts-lhncbc-lforms@v1.5.11/mod.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.2.4/mod.ts";
