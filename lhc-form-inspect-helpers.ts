65de581e114286c3953ae0c6796c7b13c6d58cc2
Shahid N. Shah <shahid@shah.org>
2020-10-20 21:32:34 -0400

    feat: add type guards for determining ValueElement types

--- lhc-form-inspect-helpers.ts
+++ lhc-form-inspect-helpers.ts
@@ -27,6 +27,103 @@ export function isLhcFormItemInspectionResult<
   )(o);
 }
 
+// @Geo or @Alan this should be merged into lf.ValueElement like this:
+// export type ValueElement = string | boolean | number | ConstrainedValue
+export type ConstrainedValue = {
+  text?: string;
+  code?: string | number;
+  other?: boolean | string;
+  system?: string | null;
+  label?: string | null;
+  score?: string | number | null;
+};
+
+export function isConstrainedValue(
+  value: lf.ValueElement | lf.ValueElement[],
+): value is ConstrainedValue {
+  switch (typeof value) {
+    case "number":
+    case "boolean":
+    case "string":
+      return false;
+  }
+  return true;
+}
+
+export function isConstrainedValues(
+  value: lf.ValueElement | lf.ValueElement[],
+): value is ConstrainedValue[] {
+  if (Array.isArray(value)) {
+    return value.find((v) => !isConstrainedValue(v)) ? false : true;
+  }
+  return false;
+}
+
+export function isNumericValue(
+  value: lf.ValueElement | lf.ValueElement[],
+): value is number {
+  switch (typeof value) {
+    case "number":
+      return true;
+
+    default:
+      return false;
+  }
+}
+
+export function isNumericValues(
+  value: lf.ValueElement | lf.ValueElement[],
+): value is number[] {
+  if (Array.isArray(value)) {
+    return value.find((v) => !isNumericValue(v)) ? false : true;
+  }
+  return false;
+}
+
+export function isBooleanValue(
+  value: lf.ValueElement | lf.ValueElement[],
+): value is number {
+  switch (typeof value) {
+    case "boolean":
+      return true;
+
+    default:
+      return false;
+  }
+}
+
+export function isBooleanValues(
+  value: lf.ValueElement | lf.ValueElement[],
+  testAll: boolean,
+): value is boolean[] {
+  if (Array.isArray(value)) {
+    return value.find((v) => !isBooleanValue(v)) ? false : true;
+  }
+  return false;
+}
+
+export function isTextValue(
+  value: lf.ValueElement | lf.ValueElement[],
+): value is string {
+  switch (typeof value) {
+    case "string":
+      return true;
+
+    default:
+      return false;
+  }
+}
+
+export function isTextValues(
+  value: lf.ValueElement | lf.ValueElement[],
+  testAll: boolean,
+): value is string[] {
+  if (Array.isArray(value)) {
+    return value.find((v) => !isTextValue(v)) ? false : true;
+  }
+  return false;
+}
+
 export function inspectRequiredFormItem<
   F extends lf.NihLhcForm = lf.NihLhcForm,
   I extends lf.FormItem = lf.FormItem,
@@ -34,16 +131,18 @@ export function inspectRequiredFormItem<
   form: F,
   item: I,
 ): lf.LhcFormInspectionIssue | I {
+  const value = item.value;
+  const isArray = Array.isArray(value);
   // TODO -- @alan or @geo check all the ways fields can miss data
-  if (!item.value) {
+  if (!value) {
     return lf.lchFormItemIssue(form, item, "Value required");
-  } else if (item.value == "" || item.value == null) {
+  } else if (value == "" || value == null) {
     return lf.lchFormItemIssue(form, item, "Value required");
-  } else if (typeof item.value === "object") {
-    /* @shahid  Not able to access text or code from item value object
-     * This may need to change in schema lhc form gsd schema lform.ts file
-     */
-    //item.value.text && item.value.code need to be validated
+  } else if (isConstrainedValues(value)) {
+    console.log("Is an array of constraints");
+  } else if (isConstrainedValue(value)) {
+    console.log(value.text);
+    console.log(value.code);
   }
 
   return item;
