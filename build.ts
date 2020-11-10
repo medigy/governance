0659a961a12f51874916dcf61a791815541ef616
Geo V L <geovlazar@outlook.com>
2020-11-10 13:23:51 +0530

    Create a build.ts  for auto generate deno service

---
+++ build.ts
@@ -0,0 +1,55 @@
+// We import build-only modules directly instead of using deps.ts to keep them out of Docker container
+import * as giac from "https://denopkg.com/gov-suite/governed-iac@v1.0.4/core/mod.ts";
+import * as iacModel from "https://denopkg.com/gov-suite/governed-iac@v1.0.4/models/service/deno.service.giac.ts";
+// import * as giac from "../../../../../github.com/gov-suite/governed-iac/core/mod.ts";
+// import * as iacModel from "../../../../../github.com/gov-suite/governed-iac/models/service/deno.service.giac.ts";
+import * as ap from "https://denopkg.com/shah/artifacts-persistence@v1.1.0/mod.ts";
+import * as cm from "https://denopkg.com/shah/context-manager@v1.0.6/mod.ts";
+import * as sm from "https://denopkg.com/shah/specification-module@v1.0.6/mod.ts";
+import { shcGH } from "./deps-test.ts";
+
+const gttRepo = shcGH.GitHub.singleton.repo(
+  { org: "medigy", repo: "governance" },
+);
+const latestTag = await gttRepo.repoLatestTag();
+if (!latestTag) {
+  throw new Error("Unable to detect latest version of governed-text-template");
+}
+const ctx = cm.ctxFactory.projectContext(".");
+const port = 8159;
+
+const mgctl =
+  `https://denopkg.com/medigy/governance@${latestTag.identity}/mgctl.ts`;
+
+giac.dockerTr.transformDockerArtifacts(
+  {
+    projectCtx: ctx,
+    name: "graph",
+    spec: sm.specFactory.spec<giac.ConfiguredServices>(
+      new iacModel.DenoServicesConfig(
+        ctx,
+        iacModel.denoServiceOptions({
+          imageTag: "medigy_governance",
+          port: port,
+          cacheURLs: ["deps.ts", mgctl],
+          entryPoint: [
+            "/deno",
+            "run",
+            "-A",
+            "--unstable",
+            mgctl,
+            "server",
+            "--verbose",
+          ],
+        }),
+      ),
+    ),
+    persist: new ap.FileSystemPersistenceHandler({
+      projectPath: ".",
+      destPath: ".",
+      report: ap.consolePersistenceResultReporter,
+    }),
+    composeBuildContext: ctx.projectPath,
+    composeYamlOptions: { lineWidth: 512 },
+  },
+);
--- deps-test.ts
+++ deps-test.ts
@@ -1,2 +1,3 @@
 // Dependencies required during engineering/testing only (not in production)
 export * as testingAsserts from "https://deno.land/std@0.76.0/testing/asserts.ts";
+export * as shcGH from "https://denopkg.com/shah/ts-safe-http-client@v0.6.15/git/github/mod.ts";
